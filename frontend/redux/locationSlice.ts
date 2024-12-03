021a4ef2bef934312d105b873885e75352d005b5
RybaLP <148885360+RybaLP@users.noreply.github.com>
2024-12-03 23:19:18 +0100

    changed some redux stuff

--- frontend/redux/locationSlice.ts
+++ frontend/redux/locationSlice.ts
@@ -1,31 +1,87 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createSlice, createAsyncThunk, PayloadAction} from "@reduxjs/toolkit";
+import { fetchHistory, createLocation, deleteLocation } from "../api/locationService";
 
 interface Location {
-    _id: string;
-    locationName: string;
+    locationName : string,
+    _id : string, 
     coordinates?: { lat: number; lng: number };
 }
 
-interface LocationsState {
-    locations: Location[];
-    error: string | null;
+interface LocationState {
+    locations : Location[],
+    loading : boolean,
+    error : string | null,
 }
 
-const initialState: LocationsState = {
-  locations: [],
-  error: null,
-};
+const initialState: LocationState = {
+    locations: [],
+    loading: false,
+    error: null,
+}
+
+export const fetchLocations = createAsyncThunk<Location[]>('locations/fetchLocations',
+    async (_, thunkAPI) => {
+        try{
+            const data = await fetchHistory();
+            return data;
+        } catch (error : any) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
+export const addNewLocation = createAsyncThunk<Location, string>(
+    'locations/addLocation',
+    async(locationName, thunkApi) => {
+        try{
+            const newLocation = await createLocation(locationName);
+            return newLocation;
+        }catch(error : any){
+            return thunkApi.rejectWithValue(error.message);
+        }
+    }
+);
+
+export const removeLocation = createAsyncThunk<string, string>(
+    'locations/deleteLocation',
+    async (id, thunkAPI) => {
+      try {
+        await deleteLocation(id);
+        return id;
+      } catch (error: any) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    }
+);
+
 
 const locationSlice = createSlice({
     name: "locations",
     initialState,
-    reducers: {
-        addLocation(state,action: PayloadAction<Location>){
+    reducers : {},
+    extraReducers : (builder) => {
+        builder
+        .addCase(fetchLocations.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(fetchLocations.fulfilled, (state, action : PayloadAction<Location[]>) =>{
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(fetchLocations.rejected, (state, action)=>{
+            state.loading = false,
+            state.error = action.payload as string;
+        })
+        .addCase(addNewLocation.fulfilled, (state, action: PayloadAction<Location>) => {
             state.locations.push(action.payload);
-        }
+        })
+        .addCase(removeLocation.fulfilled, (state, action : PayloadAction<string>)=>{
+            state.locations = state.locations.filter((loc)=>loc._id !== action.payload);
+        });
     }
 })
 
-export const {addLocation} = locationSlice.actions;
 export default locationSlice.reducer;
 
+  
